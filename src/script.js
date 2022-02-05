1eb9eeff1a8d58b4edadd6371bf3841238e1d7dc
Ashutoshm17121999 <ashutoshmishra2022jan@cedcoss.com>
2022-02-05 13:56:49 +0530

    asssignment with a returned value

--- src/script.js
+++ src/script.js
@@ -384,3 +384,15 @@ function addFive(){
 }
 addThree();
 addFive();
+
+// Assignment with a returned value
+
+
+let processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+
+processed=processArg(7);
