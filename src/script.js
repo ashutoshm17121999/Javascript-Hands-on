165d3e86bef9d9081684af227145bce27748a656
Ashutoshm17121999 <ashutoshmishra2022jan@cedcoss.com>
2022-02-05 15:16:30 +0530

    comparison with than operator

--- src/script.js
+++ src/script.js
@@ -522,3 +522,33 @@ function testNotEqual(val) {
   }
   
   testGreaterOrEqual(10);
+
+//   Comparsion with less than operator
+
+function testLessThan(val) {
+    if (val<25) { 
+      return "Under 25";
+    }
+  
+    if (val<55) { 
+      return "Under 55";
+    }
+  
+    return "55 or Over";
+  }
+  
+  testLessThan(10);
+ //   comparison with less or equal to operator
+ function testLessOrEqual(val) {
+    if (val<=12) {  
+      return "Smaller Than or Equal to 12";
+    }
+  
+    if (val<=24) {  
+      return "Smaller Than or Equal to 24";
+    }
+  
+    return "More Than 24";
+  }
+  
+  testLessOrEqual(10);
