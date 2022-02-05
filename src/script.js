4e0ad2811b49086880ebaaa142928b146bde4ad0
Ashutoshm17121999 <ashutoshmishra2022jan@cedcoss.com>
2022-02-05 15:01:13 +0530

    practise comparison with different values

--- src/script.js
+++ src/script.js
@@ -457,4 +457,14 @@ function testStrictNotEqual(val) {
   
   testStrictNotEqual(10);
 
+// Practise comparison different values
+function compareEquality(a, b) {
+    if (a === b) { 
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  compareEquality(10, "10");
+
 
