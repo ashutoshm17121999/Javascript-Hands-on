19d603d4e41ed05932ab2ebc19ebc8388e008565
Ashutoshm17121999 <ashutoshmishra2022jan@cedcoss.com>
2022-02-05 15:11:41 +0530

    comparison with greater or equal to operator

--- src/script.js
+++ src/script.js
@@ -506,3 +506,19 @@ function testNotEqual(val) {
   }
   
   testGreaterThan(10);
+
+  //   comparison with greater or equal than operator
+
+  function testGreaterOrEqual(val) {
+    if (val>=20) { 
+      return "20 or Over";
+    }
+  
+    if (val>=10) {  
+      return "10 or Over";
+    }
+  
+    return "Less than 10";
+  }
+  
+  testGreaterOrEqual(10);
