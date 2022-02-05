11398e2df1b566c436a4e7375051886604c02429
Ashutoshm17121999 <ashutoshmishra2022jan@cedcoss.com>
2022-02-05 15:40:10 +0530

    logical order if else   statement

--- src/script.js
+++ src/script.js
@@ -616,4 +616,20 @@ function testElseIf(val) {
   testElseIf(7);
 
 
+//   logical order if else statements
 
+
+
+function orderMyLogic(val) {
+    if (val < 5) {
+      return "Less than 5";
+    } else if(val < 10) {
+      return "Less than 10";
+    } else {
+      return "Greater than or equal to 10";
+    }
+  }
+  
+  orderMyLogic(7);
+
+  
