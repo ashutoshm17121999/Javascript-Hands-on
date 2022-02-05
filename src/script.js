6d8afed81e24e837d0bcde296dbb38b2e55c6b1f
Ashutoshm17121999 <ashutoshmishra2022jan@cedcoss.com>
2022-02-05 15:36:03 +0530

    introducing else if  statement

--- src/script.js
+++ src/script.js
@@ -603,5 +603,17 @@ function testElse(val) {
   
   testElse(4);
 
+//   introduce else if statement
+function testElseIf(val) {
+    if (val > 10) {
+      return "Greater than 10";
+    } else if(val < 5) {
+      return "Smaller than 5";
+    } else {
+    return "Between 5 and 10";
+  }
+  }
+  testElseIf(7);
+
 
 
