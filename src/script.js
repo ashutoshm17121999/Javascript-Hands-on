da991f2142f36ab1823b87c3f11eff05e3474ba6
Ashutoshm17121999 <ashutoshmishra2022jan@cedcoss.com>
2022-02-05 15:46:31 +0530

    changing if else statement

--- src/script.js
+++ src/script.js
@@ -631,5 +631,24 @@ function orderMyLogic(val) {
   }
   
   orderMyLogic(7);
+// changing if else statement
 
+
+function testSize(num) {
+   
+  if(num<5){
+    return "Tiny";
+  }else if(num < 10){
+    return "Small";
+  }else if(num < 15){
+    return "Medium";
+  }else if(num<20){
+    return "Large";
+  }else{
+    return "Huge";
+  }
   
+   
+  }
+  
+  testSize(7);
